e8b2704889122af826f2e7e9aee3556300f78b0b
Rui Miguel Dos Santos <rsantos@admtronics.com>
2017-06-28 18:43:15 -0400

    Added Terminal Access example to main.cpp, running ping and outputting text to debug

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+//Needed for this branch: terminal access
+#include <QDebug>
+#include <QProcess>
+
 int main(int argc, char *argv[])
 {
+    QProcess process;
+    process.start("ping 8.8.8.8 -c 2"); //Put your terminal command here
+    process.waitForFinished(-1); // will wait forever until finished, can be changed Qprocess emits a "finished" signal
+
+    QString stdout = process.readAllStandardOutput();
+    QString stderr = process.readAllStandardError();
+
+    /* Will print Terminal Output to Application output below
+     * .noquote() interprets all newlines, qDebug() will be default print out '\n' */
+    qDebug().noquote() << "stdout: " << stdout << "\n stderr: " << stderr;
+
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
